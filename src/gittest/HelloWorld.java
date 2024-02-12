4524770a59fd5f134de5f1409d222777773155bd
emu <jspark14@emich.edu>
2024-02-12 10:40:28 -0500

    updated second message

--- src/gittest/HelloWorld.java
+++ src/gittest/HelloWorld.java
@@ -3,6 +3,6 @@ package gittest;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Howdy github project");
-		System.out.println();
+		System.out.println("Alternate");
 	}
 }
